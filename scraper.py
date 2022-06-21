894e73db747468d859b760dd2651e1a1540d675b
OtuokereTobechukwu <otuokeretobechukwu@outlook.com>
2022-06-21 18:20:11 +0100

    Getting product details

--- scraper.py
+++ scraper.py
@@ -9,3 +9,38 @@ import time
 
 
 driver = webdriver.Chrome(executable_path='/Users/Toby_Py/Documents/chromedriver')
+
+def get_product_window():
+    restaurant_window = driver.window_handles[0]
+    return restaurant_window
+
+driver.get(cerave)
+
+# Get parent table containing all products
+parent_table = driver.find_element(by=By.XPATH,value='//*[@id="productSection"]/div[2]')
+time.sleep(3)
+product = parent_table.find_element(by=By.TAG_NAME, value = 'a')
+
+product_brands = []
+product_names = []
+attributes = []
+ratings = []
+
+ActionChains(driver).move_to_element(product).key_down(Keys.COMMAND).click(product).key_up(Keys.COMMAND).perform()
+driver.switch_to.window(driver.window_handles[1])
+
+time.sleep(2)
+
+product_details = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/main/div[1]/div/div[3]')
+product_attributes = driver.find_element(by=By.XPATH, value = '//*[@id="simple-tabpanel-0"]/div/div/div/div[2]')
+print('Details found')
+
+product_brand = product_details.find_element(by=By.XPATH, value='//*[@id="__next"]/main/div[1]/div/div[3]/div[1]/div[1]/p').text
+product_name = product_details.find_element(by=By.XPATH, value = '//*[@id="__next"]/main/div[1]/div/div[3]/h1').text
+rating = product_details.find_element(by=By.XPATH, value = '//*[@id="__next"]/main/div[1]/div/div[3]/div[3]/p[1]').text
+
+
+
+print(product_brand)
+print(product_name)
+
